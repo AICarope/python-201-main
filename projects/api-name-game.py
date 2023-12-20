a45641bfad00293a36c9f0e2c7c6ac752a75213a
AICarope <carope45@hotmail.com>
2023-12-20 12:03:00 -0500

    Edit GET APi Request

--- projects/api-name-game.py
+++ projects/api-name-game.py
@@ -1,1 +1,12 @@
 # Add an API call to your CLI game that assigns a name to your player.
+
+import requests
+import json 
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+response = requests.get(base_url)
+mydata = response.text
+#print(response.status_code)
+with open('data.json', 'w') as f:
+    json.dump(mydata, f, indent=4)
+
+print(response.text)
