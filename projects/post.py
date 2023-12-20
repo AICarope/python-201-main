342ef6029bd1a9fc2940c617e7fdefe9ba312d94
AICarope <carope45@hotmail.com>
2023-12-20 14:36:32 -0500

    Edit API calls POST PUT

--- projects/post.py
+++ projects/post.py
@@ -1,6 +1,6 @@
 #POST API call
 import requests
-
+import json 
 base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
 
 body = {
@@ -10,11 +10,18 @@ body = {
 }
 
 response = requests.post(base_url, json=body)
-
+mydata = response.text
 # print the status code (hopefully it's 200 which means all is well)
+with open('data.json', 'w') as f:
+    json.dump(mydata, f, indent=4)
+
+print(response.text)
 print(f"Response Code: {response.status_code}")
 
 # let's make a GET request to retrieve the new data from the server
 response = requests.get(base_url)
 # print the data - see your new record?
 print(f"Response Content: {response.content}")
+####
+
+
--- projects/put.py
+++ projects/put.py
@@ -1,7 +1,7 @@
 #PUT API call
 
 import requests
-
+import json 
 base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
 
 body = {
@@ -13,9 +13,15 @@ body = {
 # here we execute the PUT request
 response = requests.put(base_url + "/YOUR_ID", json=body)
 # print the status code (hopefully it's 200 which means all is well)
+mydata = response.text
 print(f"Response Code: {response.status_code}")
 
 # let's make a GET request to retrieve the new data from the server
 response = requests.get(base_url)
 # print the data - see your updated record?
 print(f"Response Content: {response.content}")
+with open('data.json', 'w') as f:
+    json.dump(mydata, f, indent=4)
+
+print(response.text)
+##
